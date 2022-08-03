0a5bd57683297434e2f2fff7c70c541a2f0bd045
Jose Hilario Medina Sánchez <jmedinas@emeal.nttdata.com>
2022-08-03 10:36:30 -0500

    Referenciando beans por su id, name o alias

--- spring/src/main/java/com/johimesa/spring/App.java
+++ spring/src/main/java/com/johimesa/spring/App.java
@@ -11,7 +11,9 @@ public class App {
 		
 		ApplicationContext context = new ClassPathXmlApplicationContext("spring/xml/beans.xml");
 		
-		Persona p = (Persona)context.getBean("persona");
+		//Persona p = (Persona)context.getBean("persona");//referencia al bean por su id
+		//Persona p = (Persona)context.getBean("personaBean");//referencia al bean por su name
+		Persona p = (Persona)context.getBean("personaAlias");//referencia al bean por su alias, donde en name va el id
 		
 		System.out.println(p);
 		
