c350224e55226749ce7eb0366fa7cbc2f07c0e53
Hilario <johimesa@gmail.com>
2022-08-15 09:47:28 -0500

    Interfaces - Usando la interfaz y es mas flexible

--- spring/src/main/java/com/johimesa/spring/App.java
+++ spring/src/main/java/com/johimesa/spring/App.java
@@ -3,17 +3,20 @@ package com.johimesa.spring;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import com.johimesa.beans.Jugador;
+import com.johimesa.interfaces.IEquipo;
 
 public class App {
 
 	public static void main(String[] args) {
 		
 		ApplicationContext context = new ClassPathXmlApplicationContext("spring/xml/beans.xml");
-		
+		/*
 		Jugador p = (Jugador)context.getBean("messi");//referencia al bean por su id
 		
-		System.out.println(p.getName() + " - " + p.getTeam().show());
+		System.out.println(p.getName() + " - " + p.getTeam().show());*/
+		
+		IEquipo b = (IEquipo) context.getBean("juventus");
+		System.out.println(b.show());
 		
 		((ConfigurableApplicationContext)context).close();
 	}
